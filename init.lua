baa4ecad44fb6bddbf3a1004dde1d7425c3c97bc
jnotari <jcastillo.notari@gmail.com>
2025-10-04 01:10:44 -0300

    mod color

--- init.lua
+++ init.lua
@@ -5,4 +5,4 @@ require("config.nodejs").setup({ silent = true })
 require("config.lazy")
 vim.opt.timeoutlen = 1000
 vim.opt.ttimeoutlen = 0
-vim.cmd.colorscheme("industry")
+-- vim.cmd.colorscheme("gentleman_kanagawa_blur")
