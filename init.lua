df62e4fd7d2220c46d0dca7329a4697fe8e3fe02
jnotari <jcastillo.notari@gmail.com>
2026-02-08 19:09:45 -0300

    archivos pendientes

--- init.lua
+++ init.lua
@@ -5,3 +5,9 @@ require("config.nodejs").setup({ silent = true })
 require("config.lazy")
 vim.opt.timeoutlen = 1000
 vim.opt.ttimeoutlen = 0
+
+vim.api.nvim_create_user_command("MeldCurrent", function()
+  local filename = vim.fn.expand("%") -- Obtiene el nombre del archivo actual
+  vim.fn.system("git mergetool -- " .. filename) -- Ejecuta git mergetool para ese archivo específico
+  vim.cmd("checktime") -- Recarga el archivo para ver los cambios hechos por Meld
+end, {})
